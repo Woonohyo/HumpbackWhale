6c8c2df8a53530c853bc2c8e9aaf23330629479e
woonohyo <woonohyo@nhnnext.org>
2014-08-31 13:17:28 +0900

    artifactid update

--- qna/src/main/java/net/humpbackwhale/web/HomeController.java
+++ qna/src/main/java/net/humpbackwhale/web/HomeController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
-	
 	@RequestMapping("/")
 	public String home() {
 		
